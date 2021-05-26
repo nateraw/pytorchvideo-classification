eb3fa932dbb6b27fe77e0bee100713b41564789a
Nathan Raw <nxr9266@g.rit.edu>
2021-05-26 12:09:13 -0600

    :beers: cheers

--- main.py
+++ main.py
@@ -1,32 +1,27 @@
+import torch
 import pytorch_lightning as pl
+from pytorchvideo.models import create_res_basic_head
 
-from data import UCF11
 from model import Classifier
+from data import make_ucf11_datamodule
 
 
-def main(
-    batch_size: int = 32,
-    num_workers: int = 8,
-    num_holdout_scenes: int = 2,
-    learning_rate: float = 2e-4, 
-    seed: int = 42,
-    gpus: int = 1,
-    precision: int = 16,
-    max_epochs: int = 5
-):
-    pl.seed_everything(seed)
-    data = UCF11(
-        batch_size=batch_size,
-        num_workers=num_workers,
-        num_holdout_scenes=num_holdout_scenes
-    )
-    classifier = Classifier(num_classes=data.num_classes, lr=learning_rate)
-    trainer = pl.Trainer(gpus=gpus, precision=precision, max_epochs=max_epochs)
-    trainer.fit(classifier, data)
-    return data, classifier, trainer
+def make_slowr50_finetuner(num_labels, freeze_backbone=True):
+    '''Init Pretrained Model, freeze its backbone, and replace its classification head'''
+    model = torch.hub.load("facebookresearch/pytorchvideo", "slow_r50", pretrained=True)
+    model.blocks[:-1].requires_grad_(not freeze_backbone)
+    model.blocks[-1] = create_res_basic_head(in_features=2048, out_features=num_labels)
+    return model
+
+
+def main():
+    pl.seed_everything(42)
+    dm = make_ucf11_datamodule(batch_size=8, num_workers=2)
+    model = make_slowr50_finetuner(dm.num_labels)
+    classifier = Classifier(model, lr=2e-4)
+    trainer = pl.Trainer(gpus=1, precision=16, max_epochs=4)
+    trainer.fit(classifier, dm)
 
 
 if __name__ == '__main__':
-    data, classifier, trainer= main()
-    # import typer
-    # typer.run(main)
+    main()
