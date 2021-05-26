959a659f317a9bd0090f0cea36c7494f3dfc2844
Nathan Raw <nxr9266@g.rit.edu>
2021-05-26 12:07:21 -0600

    :beers: Cheers

--- model.py
+++ model.py
@@ -1,56 +1,33 @@
-import pytorch_lightning as pl
 import torch
-from pytorchvideo.models.head import create_res_basic_head
-from torch import nn
-from torch.optim import Adam
+import pytorch_lightning as pl
+from torchmetrics import Accuracy
 
 
 class Classifier(pl.LightningModule):
-    def __init__(self, num_classes=11, lr=2e-4):
+
+    def __init__(self, model, lr: float = 2e-4, **kwargs):
         super().__init__()
-        self.save_hyperparameters()
-
-        # Backbone
-        resnet = torch.hub.load("facebookresearch/pytorchvideo", "slow_r50", pretrained=True)
-        self.backbone = nn.Sequential(*list(resnet.children())[0][:-1])
-        for param in self.backbone.parameters():
-            param.requires_grad = False
-
-        # Head
-        self.head = create_res_basic_head(in_features=2048, out_features=self.hparams.num_classes)
-
-        # Metrics
-        self.loss_fn = nn.CrossEntropyLoss()
-        self.accuracy = pl.metrics.Accuracy()
+        self.save_hyperparameters('lr', *list(kwargs))
+        self.model = model
+        self.loss_fn = torch.nn.CrossEntropyLoss()
+        self.val_acc = Accuracy()
 
     def forward(self, x):
-        if isinstance(x, dict):
-            x = x["video"]
-        feats = self.backbone(x)
-        return self.head(feats)
-
-    def shared_step(self, batch, split="train"):
-        x = batch["video"]
-        y = batch["label"]
-        y_hat = self(x)
-        loss = self.loss_fn(y_hat, y)
-        self.log(f"{split}_loss", loss)
-
-        if split in ["val", "test"]:
-            preds = y_hat.argmax(dim=1)
-            acc = self.accuracy(preds, y)
-            self.log(f"{split}_acc", acc, prog_bar=True)
-
-        return loss
+        return self.model(x)
 
     def training_step(self, batch, batch_idx):
-        return self.shared_step(batch, "train")
+        outputs = self(batch["video"])
+        loss = self.loss_fn(outputs, batch["label"])
+        self.log(f"train_loss", loss)
+        return loss
 
     def validation_step(self, batch, batch_idx):
-        return self.shared_step(batch, "val")
-
-    def test_step(self, batch, batch_idx):
-        return self.shared_step(batch, "test")
+        outputs = self(batch["video"])
+        loss = self.loss_fn(outputs, batch["label"])
+        self.log(f"val_loss", loss)
+        acc = self.val_acc(outputs.argmax(1), batch["label"])
+        self.log(f"val_acc", acc, prog_bar=True)
+        return loss
 
     def configure_optimizers(self):
-        return Adam(self.parameters(), lr=self.hparams.lr)
+        return torch.optim.Adam(self.parameters(), lr=self.hparams.lr)
